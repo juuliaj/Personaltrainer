feb4ad573a44b0d4cda3590a63fc3890b87ec1e6
Juulia Jokinen <bgm030@myy.haaga-helia.fi>
2021-05-04 15:14:29 +0300

    First task ready

--- src/components/Trainingslist.js
+++ src/components/Trainingslist.js
@@ -5,10 +5,39 @@ import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 function Trainingslist() {
 
+    const [trainings, setTrainings] = useState([]);
+
+    useEffect(() => {
+        fetchTrainings();
+    }, []);
+
+    const fetchTrainings = () => {
+        fetch('https://customerrest.herokuapp.com/api/trainings')
+        .then(response => response.json())
+        .then(data => setTrainings(data.content))
+        .catch(err => console.error(err))
+    }
+
+    const columns = [
+        { field: 'date', sortable: true, filter: true },
+        { field: 'duration', sortable: true, filter: true },
+        { field: 'activity', sortable: true, filter: true },
+    ]
+
 return(
     <div>
-        <p>sup bitch</p>
+         <div className='ag-theme-material' style={{ height: 600, width: '90%', margin: 'auto' }}>
+                    <AgGridReact
+                        rowData={trainings}
+                        columnDefs={columns}
+                        pagination={true}
+                        paginationPageSize={10}
+                        floatingFilter={true}
+                        suppressCellSelection={true}
+                    />
+        </div>
     </div>
+
     )
 }
 
