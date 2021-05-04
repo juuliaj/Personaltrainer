a17c515155469c4b6cfe9a3c1add2ca419fb56ab
Juulia Jokinen <bgm030@myy.haaga-helia.fi>
2021-05-04 15:03:09 +0300

    Firts commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Customerlist from './components/Customerlist';
+import Trainingslist from './components/Trainingslist';
+
+function App() {
+  return (
+    <div className="App">
+      <Customerlist />
+        <Trainingslist />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Trainingslist.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+
+function Trainingslist() {
+
+return(
+    <div>
+        <p>sup bitch</p>
+    </div>
+    )
+}
+
+export default Trainingslist;
